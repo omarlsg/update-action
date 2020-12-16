3a23146d77dbd397af235f120aedd7ced5e4f67f
Minka Colombia <omar@minka.io>
2020-12-16 18:23:23 -0500

    fix commander

--- src/main.js
+++ src/main.js
@@ -2,19 +2,19 @@
 require('dotenv').config({path: ".env.prd"})
 const commander = require('commander')
 const updateTarget = require('./commands/updateTarget')
-const updateLabels = require('./commands/updateTarget')
+const updateLabels = require('./commands/updateLabels')
 const withErrorHandler = require('./shared/withErrorHandler')
 
 commander
   .command('labels')
   .requiredOption('-f, --file <file>', 'file name')
-  .requiredOption('-td, --to_do <action>', 'action to execute')
+  .requiredOption('-td, --to_do <action>', 'action to execute (check/update)')
   .action(withErrorHandler(updateLabels))
 
 commander
   .command('target')
   .requiredOption('-f, --file <file>', 'file name')
-  .requiredOption('-td, --to_do <action>', 'action to execute')
+  .requiredOption('-td, --to_do <action>', 'action to execute (check/update)')
   .action(withErrorHandler(updateTarget))
 
 commander.parseAsync(process.argv)
